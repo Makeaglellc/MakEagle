b51e3be31db4058bcf2576b02dbe1af638e5fcf9
Ayesha Naeem <ayesha.naeem351999@gmail.com>
2025-08-07 00:04:54 +0200

    Updates

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 /*!
 * Start Bootstrap - Agency v7.0.12 (https://startbootstrap.com/theme/agency)
 * Copyright 2013-2025 Start Bootstrap
-* Licensed under MIT (https://github.com/StartBootstrap/https://makeaglellc.github.io/MakEagle/blob/master/LICENSE)
+* Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-agency/blob/master/LICENSE)
 */
 //
 // Scripts
